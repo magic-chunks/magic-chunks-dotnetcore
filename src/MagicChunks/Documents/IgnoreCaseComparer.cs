e0b6dac3b75deea4deddd1a04f064b2c0428d00d
Sergey Zwezdin <sergey@zwezdin.com>
2016-06-15 07:08:37 +0500

    Minor fixes

--- src/MagicChunks/Documents/IgnoreCaseComparer.cs
+++ src/MagicChunks/Documents/IgnoreCaseComparer.cs
@@ -4,15 +4,15 @@ namespace MagicChunks.Documents
 {
     public class IgnoreCaseComparer : IEqualityComparer<object>
     {
-        public bool Equals(object x, object y)
+        bool IEqualityComparer<object>.Equals(object x, object y)
         {
             if (x is string && y is string)
-                return ((string) x).ToLowerInvariant() == ((string) y).ToLowerInvariant();
+                return ((string)x).ToLowerInvariant() == ((string)y).ToLowerInvariant();
 
             return x == y;
         }
 
-        public int GetHashCode(object obj)
+        int IEqualityComparer<object>.GetHashCode(object obj)
         {
             var s = obj as string;
             return s != null ? s.ToLowerInvariant().GetHashCode() : obj.GetHashCode();
