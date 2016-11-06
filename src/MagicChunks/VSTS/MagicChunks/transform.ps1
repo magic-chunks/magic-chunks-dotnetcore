96c5b686a7706af0401754bcfd171e4cee818c1b
Sergey Zwezdin <sergey@zwezdin.com>
2016-11-06 07:59:45 +0500

    Recurse search support for VSTS extension #6

--- src/MagicChunks/VSTS/MagicChunks/transform.ps1
+++ src/MagicChunks/VSTS/MagicChunks/transform.ps1
@@ -3,6 +3,9 @@ param(
     [String] [Parameter(Mandatory = $true)] [ValidateNotNullOrEmpty()]
     $sourcePath,
 
+    [bool]
+    $sourcePathRecurse,
+
     [String] [Parameter(Mandatory = $true)] [ValidateNotNullOrEmpty()]
     $fileType,
 
@@ -19,29 +22,54 @@ param(
     $transformations
 )
 
+# Init Magic Chunks
+
 Add-Type -Path "$PSScriptRoot\MagicChunks.dll"
 
-Write-Host "Transforming file $($sourcePath)"
+
+# Parse transforms
 
 try {
     $transforms = New-Object -TypeName MagicChunks.Core.TransformationCollection `
 
-    
     foreach($t in ($transformations.Replace("\", "\\") | ConvertFrom-Json).psobject.properties) {
         $transforms.Add($t.name, $t.value)
     }
-
-    if ($targetPathType -eq "source") {
-        $target = $sourcePath;
-    }
-    elseif ($targetPathType -eq "specific") {
-        $target = $targetPath;
-    }
-
-    [MagicChunks.TransformTask]::Transform(($fileType, $null)[[string]::IsNullOrWhitespace($fileType) -or ($fileType -eq "Auto")], $sourcePath, $target, $transforms)
-
-    Write-Host "File transformed to $($target)"
 }
 catch {
-    Write-Error -Message "File transformation error: $($_.Exception.Message)" -Exception $_.Exception
+    Write-Error -Message "Transforms parsing error: $($_.Exception.Message)" -Exception $_.Exception
+    throw;
+}
+
+
+# Find files to transform
+
+if ($sourcePathRecurse) {
+    $files = Get-ChildItem $sourcePath -Recurse
+}
+else {
+    $files = Get-ChildItem $sourcePath
+}
+
+foreach ($file in $files) {
+
+    # Transform file
+
+    Write-Host "Transforming file $($file)"
+
+    try {
+        if ($targetPathType -eq "source") {
+            $target = $file;
+        }
+        elseif ($targetPathType -eq "specific") {
+            $target = $targetPath;
+        }
+
+        [MagicChunks.TransformTask]::Transform(($fileType, $null)[[string]::IsNullOrWhitespace($fileType) -or ($fileType -eq "Auto")], $sourcePath, $target, $transforms)
+
+        Write-Host "File transformed to $($target)"
+    }
+    catch {
+        Write-Error -Message "File $($file) transformation error: $($_.Exception.Message)" -Exception $_.Exception
+    }
 }
